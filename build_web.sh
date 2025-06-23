a6c5af35033363f1fe4f0c35926240cbe1d75395
WooHey-P <dkqnwlnd@naver.com>
2025-06-23 20:54:35 +0900

    sh update

--- build_web.sh
+++ build_web.sh
@@ -1,2 +1,12 @@
+#!/bin/bash
+
+# Flutter Web 빌드
 flutter build web
-cp redirects.txt build/web/_redirects
+
+# Redirect 파일 복사
+cp ./redirects.txt ./build/web/_redirects
+
+# Git 커밋 & 푸시
+git add .
+git commit -m "chore: update web build"
+git push origin main
